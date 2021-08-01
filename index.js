342be8a2b7c7c2ca4d101119edd740c94211911d
Shubham Singh <shubham88singh@gmail.com>
2021-08-02 00:28:52 +0530

    adding the dimension for the line chart

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ import * as d3 from 'd3';
 import data from './my_weather_data.json';
 
 async function drawLineChart() {
-
-    // define the accessor to enab le the access to the point after loading the data
+  // define the accessor to enab le the access to the point after loading the data
   const yAccessor = (d) => d.temperatureMax;
   const parseDate = d3.timeParse('%Y-%m-%d');
   const xAccessor = (d) => parseDate(d.date);
   console.log(xAccessor(data[0]));
+  // Defining the dimenstion of the wrapper and bounds of the graph
+  let dms = {
+    width: window.innerWidth * 0.9,
+    height: 400,
+    margin: {
+      top: 15,
+      right: 15,
+      bottom: 40,
+      left: 60,
+    },
+  };
+  dms.boundedWidth = dms.width - dms.margin.left - dms.margin.right;
+  dms.boundedHeight = dms.height - dms.margin.top - dms.margin.bottom;
+  console.log(dms);
 }
 
 drawLineChart();
