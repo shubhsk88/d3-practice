5f1c2d1310bd95d0b34ef99b64029cf56e6cb20e
Shubham Singh <shubham88singh@gmail.com>
2021-08-01 15:15:41 +0530

    Addingt eh accessor

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import * as d3 from 'd3';
+import data from './my_weather_data.json';
 
-async function drawLineChart() {}
+async function drawLineChart() {
+
+    // define the accessor to enab le the access to the point after loading the data
+  const yAccessor = (d) => d.temperatureMax;
+  const parseDate = d3.timeParse('%Y-%m-%d');
+  const xAccessor = (d) => parseDate(d.date);
+  console.log(xAccessor(data[0]));
+}
 
 drawLineChart();
