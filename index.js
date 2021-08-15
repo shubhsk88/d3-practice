7c6d571588c626d5a7e81b9b9a91f4c6ba9dfb3a
Shubham Singh <shubham88singh@gmail.com>
2021-08-15 22:51:03 +0530

    Adding the bin and yscale for histogram

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ import data from './my_weather_data.json';
 async function drawHistogram() {
   // Create the accessor
   const xAccessor = (d) => d.humidity;
+  const yAccessor = (d) => d.length;
   const width = 600;
 
-  // Define the dimensions 
+  // Define the dimensions
   const dms = {
     width,
     height: width * 0.6,
@@ -19,7 +20,39 @@ async function drawHistogram() {
   };
 
   dms.boundedHeight = dms.height - dms.margin.bottom - dms.margin.top;
-  dms.boundedWidth=dms.width-dms.margin.left-dms.margin.right
-}
+  dms.boundedWidth = dms.width - dms.margin.left - dms.margin.right;
 
+  // draw the canvas
+  const wrapper = d3
+    .select('#wrapper')
+    .append('svg')
+    .attr('width', dms.width)
+    .attr('height', dms.height)
+    .attr('fill', 'green');
+  const bound = wrapper
+    .append('g')
+    .style('transform', `translate(${dms.margin.left}px,${dms.margin.top}px)`);
+
+  // draw the scale
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(data, xAccessor))
+    .range([0, dms.boundedWidth])
+    .nice();
+
+  const binsGenerator = d3
+    .bin()
+    .domain(xScale.domain())
+    .value(xAccessor)
+    .thresholds(12);
+
+  const bins = binsGenerator(data);
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(bins, yAccessor)])
+    .range([dms.boundedHeight, 0])
+    .nice();
+
+  console.log(yScale.domain());
+}
 drawHistogram();
